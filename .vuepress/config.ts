8538acac7e0ede298a70b94743046b41141fdb96
weiyie <912881342@qq.com>
2021-11-05 14:57:19 +0800

    fix: 修改发布publicPath

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,6 +3,9 @@ import { plugins, themeConfig } from './configs'
 
 const theme = resolve('src/node/index')
 const sourceDir = resolve('docs')
+const development = process.env.NODE_ENV === 'development'
+const userName="weiyie"
+const repoName='interview'
 
 export default {
   theme, // 使用本地主题
@@ -24,4 +27,17 @@ export default {
   temp: `.vuepress/.temp`,
   cache: `.vuepress/.cache`,
   // public: '/public',
+
+  bundlerConfig: {
+    configureWebpack: (config, isServer) => {
+      // 开发模式不修改默认配置
+      if (development) return;
+  
+      config.output.publicPath = `https://cdn.jsdelivr.net/gh/${userName}/${repoName}/`
+      if (!isServer) {
+        // 修改客户端打包配置
+        config.output.filename = 'assets/js/[name].[contenthash].js'
+      }
+    }
+  }
 }
